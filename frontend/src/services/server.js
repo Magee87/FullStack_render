253255b0c65d70a00481743832147427bdf5252f
Jesse Kankaisto <jesse.kankaisto@gmail.com>
2023-05-20 22:27:18 +0300

    added number validator

--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const baseUrl = 'https://fullstack-phonebook-qfi1.onrender.com/api/persons';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://fullstack-phonebook-qfi1.onrender.com/api/persons';
+//const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
